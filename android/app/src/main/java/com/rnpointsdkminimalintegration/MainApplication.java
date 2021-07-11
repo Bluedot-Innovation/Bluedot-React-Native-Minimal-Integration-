8d45f1d73df4c4d28375b5705cfbe04ba46894f5
poborin <pavel.oborin@gmail.com>
2021-07-12 09:53:33 +1000

    Po/bd 3045 appcenter crashes (#18)
    * install appcenter dependencies
    
    * appcenter crashes for iOS
    
    * update appcenter secret
    
    * appcenter crashes for android
    
    * appcenter secret for android

--- android/app/src/main/java/com/rnpointsdkminimalintegration/MainApplication.java
+++ android/app/src/main/java/com/rnpointsdkminimalintegration/MainApplication.java
@@ -2,16 +2,20 @@ package com.rnpointsdkminimalintegration;
 
 import android.app.Application;
 import com.dieam.reactnativepushnotification.ReactNativePushNotificationPackage;
+
 import com.facebook.react.ReactApplication;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
 import com.facebook.react.shell.MainReactPackage;
 import com.facebook.soloader.SoLoader;
+import com.microsoft.appcenter.reactnative.appcenter.AppCenterReactNativePackage;
+import com.microsoft.appcenter.reactnative.crashes.AppCenterReactNativeCrashesPackage;
 import com.reactnativecommunity.geolocation.GeolocationPackage;
 import com.rnpointsdkminimalintegration.generated.BasePackageList;
 import com.swmansion.gesturehandler.react.RNGestureHandlerPackage;
 import com.swmansion.reanimated.ReanimatedPackage;
 import com.swmansion.rnscreens.RNScreensPackage;
+import com.zoontek.rnpermissions.RNPermissionsPackage;
 import io.bluedot.BluedotPointSdkPackage;
 import java.util.Arrays;
 import java.util.List;
@@ -35,8 +39,11 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
+            new AppCenterReactNativeCrashesPackage(MainApplication.this, getResources().getString(R.string.appCenterCrashes_whenToSendCrashes)),
+            new AppCenterReactNativePackage(MainApplication.this),
             new GeolocationPackage(),
             new ReactNativePushNotificationPackage(),
+            new RNPermissionsPackage(),
             new BluedotPointSdkPackage(),
           new ReanimatedPackage(),
           new RNGestureHandlerPackage(),
