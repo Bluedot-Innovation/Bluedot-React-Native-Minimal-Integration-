a02798e3e8fb4ae39ff4582ce04c360dd15a57ef
Neha Ishwar <neha@bluedot.io>
2024-04-10 16:32:40 +1000

    updated metro ,expo and gradle versions to be compatible with sdk16

--- metro.config.js
+++ metro.config.js
@@ -1,3 +1,5 @@
+const {getDefaultConfig, mergeConfig} = require('@react-native/metro-config');
+
 /**
  * Metro configuration for React Native
  * https://github.com/facebook/react-native
@@ -5,13 +7,18 @@
  * @format
  */
 
-module.exports = {
-  transformer: {
+module.exports = (() => {
+
+  const config = getDefaultConfig(__dirname);
+
+  const { transformer } = config;
+  config.transformer = {
     getTransformOptions: async () => ({
       transform: {
         experimentalImportSupport: false,
         inlineRequires: true,
       },
     }),
-  },
-};
+  };
+  return config;
+})();
