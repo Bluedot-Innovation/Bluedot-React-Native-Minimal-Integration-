61b7d1ea40838546c96d5e8fe5a95db567b5450d
Neha Ishwar <neha@bluedot.io>
2024-04-11 13:16:25 +1000

    Babel core version updated

--- metro.config.js
+++ metro.config.js
@@ -8,17 +8,6 @@ const {getDefaultConfig, mergeConfig} = require('@react-native/metro-config');
  */
 
 module.exports = (() => {
-
   const config = getDefaultConfig(__dirname);
-
-  const { transformer } = config;
-  config.transformer = {
-    getTransformOptions: async () => ({
-      transform: {
-        experimentalImportSupport: false,
-        inlineRequires: true,
-      },
-    }),
-  };
   return config;
 })();
